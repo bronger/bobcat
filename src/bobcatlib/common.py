86de78e3258fa1042b37f423963eaefc446f9e65
Torsten Bronger <bronger@physik.rwth-aachen.de>
2008-04-14 16:43:58 +0200

    Shortened licence in all source files and moved it to COPYING.

--- src/bobcatlib/common.py
+++ src/bobcatlib/common.py
@@ -5,26 +5,11 @@
 #
 #    This file is part of the Bobcat program.
 #
-#    Bobcat is free software; you can redistribute it and/or modify it under
-#    the terms of the MIT licence:
+#    Bobcat is free software; you can use it, redistribute it and/or modify it
+#    under the terms of the MIT license.
 #
-#    Permission is hereby granted, free of charge, to any person obtaining a
-#    copy of this software and associated documentation files (the "Software"),
-#    to deal in the Software without restriction, including without limitation
-#    the rights to use, copy, modify, merge, publish, distribute, sublicense,
-#    and/or sell copies of the Software, and to permit persons to whom the
-#    Software is furnished to do so, subject to the following conditions:
-#
-#    The above copyright notice and this permission notice shall be included in
-#    all copies or substantial portions of the Software.
-#
-#    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-#    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-#    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-#    DEALINGS IN THE SOFTWARE.
+#    You should have received a copy of the MIT license with Bobcat.  If not,
+#    see <http://bobcat.origo.ethz.ch/wiki/Licence>.
 #
 
 """Module for things that are used by (almost) all parts of Bobcat.  This is
