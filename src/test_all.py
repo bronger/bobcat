d870b761cc5fd746a86899cb7697f6f142d96df5
Torsten Bronger <bronger@physik.rwth-aachen.de>
2008-04-16 20:36:04 +0200

    Added docstring and --all option.

--- src/test_all.py
+++ src/test_all.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import unittest
+"""Script for executing all tests of the Bobcat program.  This is the script
+which is executed my the "make tests" target in Bobcat's main Makefile.
+
+If you pass ``--all`` to this script, it also tests all doctests in (almost)
+all source code files and does further things.  Without ``--all``, it just runs
+all test suites for the modules.
+"""
+
+import unittest, sys
 from tests import common_test
 
+# Build test suite
 suite = unittest.TestSuite()
-
 from tests import test_common, test_helpers, test_preprocessor, test_settings, test_safefilename, \
     test_i18n, test_emitter, test_latex_substitutions
 suite.addTests([test_common.suite,
@@ -16,7 +24,12 @@ suite.addTests([test_common.suite,
                 test_i18n.suite,
                 test_emitter.suite,
                 test_latex_substitutions.suite])
+if len(sys.argv) > 1 and sys.argv[1] == "--all":
+    from tests import test_doctests
+    suite.addTest(test_doctests.suite)
+    # FixMe: Add epydoc generating test, and document processing tests
 
+# Run the tests
 common_test.chdir_to_testbed()
 runner = unittest.TextTestRunner()
 runner.run(suite)
