94388e84c4752fd6187dc55b0fa442bc4a37e5c1
Torsten Bronger <bronger@physik.rwth-aachen.de>
2008-04-20 22:58:04 +0200

    Created TestParseLocalVariables test case (no tests yet).

--- src/tests/test_common.py
+++ src/tests/test_common.py
@@ -6,4 +6,12 @@ from bobcatlib import common
 
 suite = unittest.TestSuite()
 
+class TestParseLocalVariables(unittest.TestCase):
+    def shortDescription(self):
+        description = super(TestParseLocalVariables, self).shortDescription()
+        return "common.parse_local_variables: " + (description or "")
+
+for test_class in (TestParseLocalVariables):
+    suite.addTests(unittest.defaultTestLoader.loadTestsFromTestCase(test_class))
+
 suite.addTest(doctest.DocFileSuite("common.txt"))
