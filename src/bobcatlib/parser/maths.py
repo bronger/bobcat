fa89c62b4ee09a3714ae34fcd2e4878cee6a6930
Torsten Bronger <bronger@physik.rwth-aachen.de>
2008-05-12 21:58:05 +0200

    Added some helper variables/functions.

--- src/bobcatlib/parser/maths.py
+++ src/bobcatlib/parser/maths.py
@@ -13,6 +13,7 @@
 #
 
 from .common import guarded_match, guarded_search, guarded_find, Node
+import re
 
 superscripts = {u"⁰": u"0", u"¹": u"1", u"²": u"2", u"³": u"3", u"⁴": u"4",
                 u"⁵": u"5", u"⁶": u"6", u"⁷": u"7", u"⁸": u"8", u"⁹": u"9",
@@ -24,13 +25,24 @@ subscripts = {u"₀": u"0", u"₁": u"1", u"₂": u"2", u"₃": u"3", u"₄": u"
               u"₊": u"+", u"₋": u"−", u"₌", u"=", u"₍": u"(", u"₎": u")",
               u"ₐ": u"a", u"ₑ": u"e", u"ₒ": u"o", u"ₓ": u"x", u"ₔ": u"ə"}
 
-opening_braces = u"([{⟨⟦⟪⟬⟮|‖"
-closing_braces = u")]}⟩⟧⟫⟭⟯"
+opening_braces = set(u"([{⟨⟦⟪⟬⟮|‖")
+closing_braces = set(u")]}⟩⟧⟫⟭⟯")
 
-breaking_whitespace = u" " + u"".join([unichr(i) for i in range(0x2000, 0x200b)]) + u"\x205f"
-nonbreaking_whitespace = u" \x202f"
-whitespace = breaking_whitespace + nonbreaking_whitespace
+breaking_whitespace = set(u" " + u"".join([unichr(i) for i in range(0x2000, 0x200b)]) + u"\x205f")
+nonbreaking_whitespace = set(u" \x202f")
+whitespace = breaking_whitespace | nonbreaking_whitespace
 
+big_operators = set(u"∏∐∑⋀⋁⋂⋃" + u"".join([unichr(i) for i in range(0x2a00, 0x2a22)]))
+
+def is_letter(character):
+    return unicodedata.category(character) in ["Lu", "Ll", "Lt", "Lo"]
+
+number_pattern = None
+def set_decimal_point(character):
+    global number_pattern
+    assert character in [u",", u"."]
+    number_pattern = re.compile(u"(+|−|-)?[0-9]+(%s[0-9]+)?(e(+|−|-)?[0-9]+)?" % re.escape(character))
+set_decimal_point(u".")
 
 def parse_math_row(parent, text, position, end):
     return guarded_find("}", text, position, end)
