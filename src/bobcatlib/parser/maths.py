716ecc597016b31d6e7819b1ed5b991a112d3fc7
Torsten Bronger <bronger@physik.rwth-aachen.de>
2008-05-24 18:52:35 +0200

    Added IdentifierSet.  Added sets for relational operators and arrows.

--- src/bobcatlib/parser/maths.py
+++ src/bobcatlib/parser/maths.py
@@ -28,6 +28,23 @@ subscripts = {u"₀": u"0", u"₁": u"1", u"₂": u"2", u"₃": u"3", u"₄": u"
 opening_braces = set(u"([{⟨⟦⟪⟬⟮|‖")
 closing_braces = set(u")]}⟩⟧⟫⟭⟯")
 
+relational_operators = set(u"<=>"
+                           + u"∴∵∻∼∽∾"
+                           + u"".join([unichr(i) for i in range(0x2241, 0x228c)])
+                           + u"⊏⊐⊑⊒"
+                           + u"".join([unichr(i) for i in range(0x22a6, 0x22ba)])
+                           + u"⋈⋍"
+                           + u"".join([unichr(i) for i in range(0x22d4, 0x2300)])
+                           + u"".join([unichr(i) for i in range(0x29e3, 0x29e7)])
+                           + u"".join([unichr(i) for i in range(0x2a66, 0x2ade)])
+                           + u"".join([unichr(i) for i in range(0x2af7, 0x2afc)]))
+arrows = set(u"".join([unichr(i) for i in range(0x2190, 0x2200)])
+             u"".join([unichr(i) for i in range(0x27f0, 0x2800)])
+             u"".join([unichr(i) for i in range(0x2900, 0x2980)])
+             u"".join([unichr(i) for i in range(0x2b00, 0x2b12)])
+             u"".join([unichr(i) for i in range(0x2b30, 0x2b4d)]))
+relational_and_arrows = relational_operators | arrows
+
 breaking_whitespace = set(u" " + u"".join([unichr(i) for i in range(0x2000, 0x200b)]) + u"\x205f")
 nonbreaking_whitespace = set(u" \x202f")
 whitespace = breaking_whitespace | nonbreaking_whitespace
@@ -35,7 +52,7 @@ whitespace = breaking_whitespace | nonbreaking_whitespace
 big_operators = set(u"∏∐∑⋀⋁⋂⋃" + u"".join([unichr(i) for i in range(0x2a00, 0x2a22)]))
 
 def is_letter(character):
-    return unicodedata.category(character) in ["Lu", "Ll", "Lt", "Lo"]
+    return unicodedata.category(character) in ["Lu", "Ll", "Lo", "Lt"]
 
 number_pattern = None
 def set_decimal_point(character):
@@ -46,6 +63,28 @@ set_decimal_point(u".")
 
 def parse_math_row(parent, text, position, end):
     return guarded_find("}", text, position, end)
+
+class IdentifierSet(object):
+    whitespace_re = u"[%s]+" % re.escape(u"".join(whitespace))
+    def __init__(self, identifiers=()):
+        self.identifiers = set(identifiers)
+    def add(self, identifier):
+        """The identifier must be whitespace-normalised!"""
+        self.identifiers.add(identifier)
+        self.pattern = None
+    def match(self, excerpts, pos=0):
+        if not self.pattern:
+            regular_expression = u"|".join(re.escape(self.identifiers))
+            regular_expression.replace(u" ", whitespace_re)
+            self.pattern = re.compile(regular_expression, re.UNICODE)
+        return self.pattern.match(unicode(excerpt), pos)
+builtin_functions = IdentifierSet(
+    u"sin", u"cos", u"tan", u"cot", u"sec", u"csc", u"arcsin", u"arccos", u"arctan", u"arccot",
+    u"arcsec", u"arccsc", u"sinh", u"cosh", u"tanh", u"coth", u"arg", u"deg", u"dim", u"exp",
+    u"lg", u"ln", u"log", u"mod", u"hom", u"ker", u"sgn")
+builtin_big_identifiers = IdentifierSet(
+    u"lim inf", u"lim sup", u"max", u"min", u"inf", u"sup", u"Pr", u"gcd", u"det", u"lim")
+builtin_identifiers = IdentifierSet()
 
 class MathEquation(Node):
     def __init__(self, parent):
